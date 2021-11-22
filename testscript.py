d0fdc8300987ad7e969370059b963f0d72708c60
Lonzarich <lglonzaric@gmail.com>
2021-11-21 20:33:40 -0600

    bookkeeping

--- testscript.py
+++ testscript.py
@@ -18,112 +18,6 @@ def get_keys():
     return keys
 
 
-### The working implementation of 'read_tracked_particles' uses tracked_particles_v2.hdf5 instead of tracked_particles.hdf5. ###
-
-################################################
-# def read_tracked_particles(sim, haloid, verbose=False):
-    
-#     if verbose: print(f'Loading tracked particles for {sim}-{haloid}...')
-    
-#     key = f'{sim}_{str(int(haloid))}'
-
-#     # import the tracked particles dataset
-#     path1 = '/home/lonzaric/astro_research/Stellar_Feedback_Code/SNeData/tracked_particles.hdf5'
-#     data = pd.read_hdf(path1, key=key)
-    
-#     time = np.unique(data.time)
-#     dt = time[1:]-time[:-1]
-#     dt = np.append(dt[0], dt)
-#     dt = dt[np.unique(data.time, return_inverse=True)[1]]
-#     data['dt'] = dt
-    
-    
-#     if verbose: print('Successfully loaded')
-    
-#     r_gal = np.array([])
-#     for t in np.unique(data.time):
-#         d = data[data.time==t]
-#         r_gas = np.mean(d.sat_r_gas)
-#         r_half = np.mean(d.sat_r_half)
-#         rg = np.max([r_gas,r_half])
-
-#         if np.isnan(rg):
-#             rg = r_gal_prev
-
-#         if verbose: print(f't = {t:1f} Gyr, satellite R_gal = {rg:.2f} kpc')
-#         r_gal = np.append(r_gal,[rg]*len(d))
-
-#         r_gal_prev = rg
-
-#     data['r_gal'] = r_gal
-    
-#     r_gal_prev = 0
-#     r_gal = np.array([])
-#     for t in np.unique(data.time):
-#         d = data[data.time==t]
-#         r_gas = np.mean(d.host_r_gas)
-#         r_half = np.mean(d.host_r_half)
-#         rg = np.max([r_gas,r_half])
-
-#         if np.isnan(rg):
-#             rg = r_gal_prev
-
-#         if verbose: print(f't = {t:1f} Gyr, host R_gal = {rg:.2f} kpc')
-#         r_gal = np.append(r_gal,[rg]*len(d))
-
-#         r_gal_prev = rg
-
-#     data['host_r_gal'] = r_gal
-    
-#     thermo_disk = (np.array(data.temp) < 1.2e4) & (np.array(data.rho) > 0.1)
-    
-#     in_sat = np.array(data.in_sat)
-#     other_sat = np.array(data.in_other_sat)
-#     in_host = np.array(data.in_host) & ~in_sat & ~other_sat
-    
-#     sat_disk = in_sat & thermo_disk
-#     sat_halo = in_sat & ~thermo_disk
-    
-#     host_disk = in_host & thermo_disk
-#     host_halo = in_host & ~thermo_disk
-    
-#     IGM = np.array(data.in_IGM)
-    
-    
-# #    sat_disk = in_sat & (np.array(data.r) <= np.array(data.r_gal))
-# #     sat_halo = in_sat & (np.array(data.r) > np.array(data.r_gal))
-# #     sat_cool_disk = sat_disk & thermo_disk
-# #     sat_hot_disk = sat_disk & ~thermo_disk
-# #     sat_cool_halo = sat_halo & thermo_disk
-# #     sat_hot_halo = sat_halo & ~thermo_disk
-
-# #     in_host = np.array(data.in_host) & ~in_sat
-# #     host_disk = in_host & (np.array(data.r_rel_host) <= np.array(data.host_r_gal))
-# #     host_halo = in_host & (np.array(data.r_rel_host) > np.array(data.host_r_gal))
-
-# #     other_sat = np.array(data.in_other_sat)
-# #     IGM = np.array(data.in_IGM)
-    
-    
-#     # basic classifications
-#     data['sat_disk'] = sat_disk
-#     data['sat_halo'] = sat_halo
-#     data['host_disk'] = host_disk
-#     data['host_halo'] = host_halo
-#     data['other_sat'] = other_sat
-#     data['IGM'] = IGM
-    
-#     # more advanced classifications
-#     #data['cool_disk'] = sat_cool_disk
-#     #data['hot_disk'] = sat_hot_disk
-#     #data['cool_halo'] = sat_cool_halo
-#     #data['hot_halo'] = sat_hot_halo
-
-#     return data
-################################################
-
-
-
 def read_tracked_particles(sim, haloid, verbose=False):
     
     if verbose: print(f'Loading tracked particles for {sim}-{haloid}...')
@@ -222,25 +116,6 @@ def calc_angles(d):
         
     return d
 
-
-
-def calc_angles_tidal(d):
-    # get gas particle velocity
-    v = np.array([d.vx,d.vy,d.vz])
-
-    # instead of CGM velocity, get vector pointing from satellite to host (i.e. host position in the satellite rest frame) 
-    r_sat =np.array([d.sat_Xc,d.sat_Yc,d.sat_Zc])
-    r_host = np.array([d.host_Xc,d.host_Yc,d.host_Zc])
-    r_rel = r_host - r_sat
-
-    # take the dot product and get the angle, in degrees
-    v_hat = v / np.linalg.norm(v)
-    r_rel_hat = r_rel / np.linalg.norm(r_rel)
-    angle = np.arccos(np.dot(v_hat,r_rel_hat)) * 180/np.pi
-
-    d['angle_tidal'] = angle
-        
-    return d
 
 
 def calc_ejected_expelled(sim, haloid, save=True, verbose=True):
@@ -382,7 +257,7 @@ def read_ram_pressure(sim, haloid):
     # loading ram pressure data for specified simulation and haloid.
     path = '/home/lonzaric/astro_research/Stellar_Feedback_Code/SNeData/ram_pressure.hdf5'
     key = f'{sim}_{haloid}'
-    data = pd.read_hdf(path, key=key)
+    data = pd.read_hdf('/home/lonzaric/astro_research/Stellar_Feedback_Code/SNeData/ram_pressure.hdf5', key=key)
     
     # converting data to numpy arrays (i.e. remove pynbody unit information) and calculating ratio
     data['Pram_adv'] = np.array(data.Pram_adv,dtype=float)
@@ -481,13 +356,13 @@ def read_ram_pressure(sim, haloid):
  
     
 def read_all_ram_pressure():
-    data_all = pd.DataFrame();
+    data_all = pd.DataFrame()
     
     keys = ['h148_13','h148_28','h148_37','h148_45','h148_68','h148_80','h148_283',
             'h148_278','h148_329','h229_20','h229_22','h229_23','h229_27','h229_55',
             'h242_24','h242_41','h242_80','h329_33','h329_137']
     
-    i = 1;
+    i = 1
     for key in keys:
         print(i, end=' ')
         i += 1
