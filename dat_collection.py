a66a8cf71016ca4329db107baaeee318a7e67296
Leo Lonzarich <lonzaric@grinnell.edu>
2022-01-30 17:16:26 -0600

    s22 update

--- dat_collection.py
+++ dat_collection.py
@@ -1,4 +1,5 @@
-# script collecting extra gas mass for sne feedback analysis
+# script collecting extra gas mass for sne feedback analysis.
+# selecting low-mass and unquenched satellites.
 
 import numpy as np
 import pandas as pd
