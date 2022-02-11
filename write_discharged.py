618a7ef8deb2a778c9311c55caf5501d2b34a142
Lonzarich <lglonzaric@gmail.com>
2022-02-10 20:32:40 -0600

    dat r/w ad.

--- write_discharged.py
+++ write_discharged.py
@@ -11,7 +11,8 @@ from compiler import *
 
 keys = ['h148_13','h148_28','h148_37','h148_45','h148_68','h148_80','h148_283',
         'h148_278','h148_329','h229_20','h229_22','h229_23','h229_27','h229_55',
-        'h242_24','h242_41','h242_80','h329_33','h329_137']
+        'h242_24','h242_41','h242_80','h329_33','h329_137','h242_12','h242_30',
+        'h242_44','h148_3','h148_14','h148_36']
 
 print('Compiling sim gas into sets (predischarged, discharged, accreted) for the following keys:', keys)
 
